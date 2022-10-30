7111b5d84c8cc9f5a06c8c8a52f028eef2a8e158
vik-wed <wedel.viktoria@gmail.com>
2022-10-30 12:57:32 -0400

    API connected and added random user generator functionality to drop down

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector(".num-users");
+
+
+
+async function getData(numUsers){
+    let usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`); 
+    let data = await usersRequest.json();
+    let userResults = data.results;
+
+    displayUsers(userResults);
+}
+getData(1);
+
+function displayUsers(userResults){
+    randomFolks.innerHTML = "";
+    for (let user of userResults){
+        let country = user.location.country;
+        let name = user.name.first;
+        let imageUrl = user.picture.medium;
+        let userDiv = document.createElement("div");
+
+        userDiv.innerHTML = `
+                <h3>${name}</h3> 
+                <p>${country}</p> 
+                <img src =${imageUrl} alt="User avatar"/>
+            `; 
+        randomFolks.append(userDiv);
+    }
+
+};
+
+selectUserNumber.addEventListener("change", function(e){
+    let numUsers = e.target.value;
+
+    getData(numUsers);
+})
